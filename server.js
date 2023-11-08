082614e4c0c2a757e01dda9ab1d1b02c300f7d17
Daniel Culpepper <culpepperd2490@gmail.com>
2023-11-07 22:40:57 -0500

    added connect function and uri

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const uri = 'mongodb+srv://MyYoutubeVideo:MyYoutubeVideoPassword@youtubevideocluster.iqcvkvl.mongodb.net/?retryWrites=true&w=majority';
+
+async function connect() {
+    try {
+        await mongoose.connect(uri)
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+connect();
+
 app.listen(8000, () => {
     console.log("Server started on port 8000");
 });
